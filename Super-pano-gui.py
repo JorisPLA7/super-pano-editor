887340da69a3128e78271609be1ba0c95147582e
tuks <joris@placette.fr>
2017-03-31 22:26:50 +0200

    Revert "A"
    This reverts commit 597f2de3971dca7e9f0e639bc841ec6ce6804ad6.

--- Super-pano-gui.py
+++ Super-pano-gui.py
@@ -1,6 +1,6 @@
 ##PROGRAMME SOUS LISCENCE G.P.L ........ Joris Placette ........ 2017
 
-global appVersion #Vaiiable contenant le numero de version du porgramme (écrit avec les données)
+global appVersion
 
 global palette
 
@@ -8,8 +8,8 @@ global posxmax
 
 comValue =0
 posxmax = 200
-appVersion = "0.3.6 : Serializeu (commenté)"
-helpPage = "https://github.com/Kouskali/super-pano-editor/blob/master/README.md" #lien pages d'aide à consulter
+appVersion = "0.3.6 : Serialize"
+helpPage = "https://github.com/Kouskali/super-pano-editor/blob/master/README.md"
 githubPage = "https://github.com/Kouskali/super-pano-editor/"
 
 cacheData = {
@@ -17,25 +17,22 @@ cacheData = {
 "angletotal":   0,
 "posx":   0,
 "versys":   appVersion,
-}# cachedata , données fournies par l'utilisateur, en attente d'être envoyées à la carte ou enregistrées
+}
 
-##boutons
-def rbfcbutton():  #fonction appelée pour ouvrir un fichier existant
+##buttons
+def rbfcbutton():
    global cacheData
    datatampon = cacheData
    cacheData = datasheets.pickread()
    if cacheData["versys"] == "fail!":
-      print("Erreur: Fichier ouvert mais lu sans succès")
+      cacheData = datatampon
    print("nouvelles données en ram: {}".format(cacheData))
-   #saisieangletotal.icursor(cacheData["angletotal"])
-   #saisieangleinter.icursor(cacheData["angleinter"])
-   #saisieposx.icursor(cacheData["posx"])
 
 def wbfcbutton():
-   datasheets.pickwrite(cacheData) # se référer à datasheets.py
+   datasheets.pickwrite(cacheData)
 
 
-def verifbutton(): #vérification des données fournies par l'utilisateur
+def verifbutton():
    nberror = 0
    if int(saisieangleinter.get()) < 0 or int(saisieangleinter.get()) > 50:
       guimessage("red", "Il y a un problème :'(  !", "l'angle entre 2 positions doit être compris entre 0 et 50° !")
@@ -48,34 +45,36 @@ def verifbutton(): #vérification des données fournies par l'utilisateur
       guimessage("red", "Il y a un problème :'(  !", "La position linéaire doit être comprise entre 0 et {} !".format(posxmax))
       nberror +=1
 
-   if nberror == 0: #si tout est valide
+   if nberror == 0:
       pulldata()
       refreshcanvas(cacheData)
       guivalidation()
-      print("Les informations saisies ne contiennes visiblement pas d' erreures")
 
 def serialsetbbutton():
    serializer.establish(comValue, cacheData)
 def serialsendbutton():
    serialzer.send(cacheData)
 
-#réccupération des données de l'utilisateur
+##data treatment
 def pulldata():
    cacheData["angletotal"] = saisieangletotal.get()
    cacheData["angleinter"] = saisieangleinter.get()
    cacheData["posx"] = saisieposx.get()
 
-def donothing(): #ne fait rien, comme son nom l'indique
+##external fcts
+def donothing():
    filewin = Toplevel(root)
    button = Button(filewin, text="Do nothing button")
-   button.pack() #on intègre le module déclaré à sa fenêtre (pack(sans paramètre) donc simplement à la suite du reste)
+   button.pack()
    print("Fenêtre qui ne fait rien ouverte")
-
 
 def forcesave():
    print("tentative de sauvegarde forcée")
    pulldata()
    wbfcbutton()
+
+
+
 
 ##gui refreshers
 def guimessage(color, context, reason):
@@ -86,7 +85,7 @@ def guimessage(color, context, reason):
    destroybutton.pack(side=LEFT)
 
    left = Label(messageframe, text=reason, fg=color)
-   left.pack() #on intègre le module déclaré à sa fenêtre (pack(sans paramètre) donc simplement à la suite du reste)
+   left.pack()
 
    root.mainloop()
 
@@ -98,85 +97,63 @@ def guivalidation():
    destroybutton.pack(side=LEFT)
 
    savebutton= Button(validationframe, text="enregistrer", command=wbfcbutton, fg="green")
-   savebutton.pack() #on intègre le module déclaré à sa fenêtre (pack(sans paramètre) donc simplement à la suite du reste)
+   savebutton.pack()
 
    desc= Label(validationframe, text="posx: {}  angle total: {}  angle intermidiaire: {}".format(cacheData["posx"],cacheData["angletotal"],cacheData["angleinter"]), fg="green")
-   desc.pack() #on intègre le module déclaré à sa fenêtre (pack(sans paramètre) donc simplement à la suite du reste)
+   desc.pack()
    root.mainloop()
 
 
 ## init tkinter
-#importation des bibliotheques pyhton
-try:
-    from tkinter import *
-    print("bibliothèque importée avec succès tkinter")
-except:
-    print("Impossible d'importer la bibliothèque :  tkinter")
-try:
-    from tkinter.filedialog import *
-    print("bibliothèque importée avec succès tkinter")
-except:
-    print("Impossible d'importer la bibliothèque :  tkinter")
-try:
-    from tkinter.messagebox import askokcancel, askyesno,askquestion
-    print("bibliothèque importée avec succès tkinter")
-except:
-    print("Impossible d'importer la bibliothèque :  tkinter")
-try:
-    import pickle
-    print("bibliothèque importée avec succès pickle")
-except:
-    print("Impossible d'importer la bibliothèque :  pickle")
-try:
-    from lib import graphiti
-    print("bibliothèque importée avec succès lib\graphiti")
-except:
-    print("Impossible d'importer la bibliothèque :  lib\graphiti")
-try:
-    from lib import web
-    print("bibliothèque importée avec succès lib\web")
-except:
-    print("Impossible d'importer la bibliothèque :  lib\web")
-try:
-    from lib import datasheets
-    print("bibliothèque importée avec succès lib\datasheets")
-except:
-    print("Impossible d'importer la bibliothèque :  lib\datasheets")
-try:
-    from lib import serializer
-    print("bibliothèque importée avec succès lib\serializer")
-except:
-    print("Impossible d'importer la bibliothèque :  lib\ser")
-root=Tk() #création de la fenêtre tkinter racine
+from tkinter import *
+from lib import graphiti
+from lib import web
+from lib import datasheets
+from lib import serializer
+from tkinter.filedialog import *
+import pickle
+from tkinter.messagebox import askokcancel, askyesno,askquestion
+root=Tk()
 
-root.wm_title('Super Pano GUI')#definition du titre
-root.wm_iconbitmap('ressources\supano.ico')#definition de l'icone
+root.wm_title('Super Pano GUI')
+root.wm_iconbitmap('ressources\supano.ico')
 
 
 
-##Barre de Menu suppérieur
+##Menubar
 menubar = Menu(root)
 
-filemenu = Menu(menubar, tearoff=0) #sous menu
+filemenu = Menu(menubar, tearoff=0)
 
 filemenu.add_command(label="Ouvrir une sauvegarde", command=rbfcbutton)
 filemenu.add_command(label="Sauvegarder sans validation", command=forcesave)
 filemenu.add_separator()
 
-serialmenu = Menu(filemenu) #sous² menu
+serialmenu = Menu(filemenu)
 for i in range (0,10):
+   #currentcomcommand = setserialcom
    serialmenu.add_radiobutton(label="Ouvrir une lisaison série COM{}".format(i), variable=comValue, value=i, command=serializer.establish)
 filemenu.add_cascade(label="Etablir une liaison série", menu=serialmenu)
+
 filemenu.add_separator()
 filemenu.add_command(label="Quitter", command=root.destroy)
 menubar.add_cascade(label="Fichier", menu=filemenu)
 
-helpmenu = Menu(menubar, tearoff=0) #sous menu
+
+
+
+
+
+
+
+
+
+helpmenu = Menu(menubar, tearoff=0)
 helpmenu.add_command(label="Version de l'application : {}".format(appVersion), command=web.help)
 helpmenu.add_command(label="Ouvrir une aide sur le web", command=web.help)
 menubar.add_cascade(label="Aide", menu=helpmenu)
 
-devmenu = Menu(menubar, tearoff=0) #sous menu
+devmenu = Menu(menubar, tearoff=0)
 # devmenu.add_command(label="fonction print test", command=graphiti.joris)
 # devmenu.add_command(label="pick write", command=verifbutton)
 # devmenu.add_command(label="pick read", command=rbfcbutton)
@@ -184,17 +161,17 @@ menubar.add_cascade(label="Developpement", menu=devmenu)
 
 root.config(menu=menubar)
 
-##Titre
+##header
 header = Label(root, text="Outil de configuration du trépied. Version {}".format(appVersion))
 header.pack(fill="both", expand="no")
 
-##Panneau lateral
+##aside frame
 aside = Frame(root)
 aside.pack(side=LEFT)
 
 ##init canvas
 w = Canvas(aside, width=200, height=300)
-w.pack() #on intègre le module déclaré à sa fenêtre (pack(sans paramètre) donc simplement à la suite du reste)
+w.pack()
 
 def refreshcanvas(cacheData):
    w.delete("all")
@@ -202,26 +179,28 @@ def refreshcanvas(cacheData):
 
    posxdisp = int(int(cacheData["posx"]) / posxmax * 180 +10)
 
-   w.create_line(posxdisp, 25, posxdisp, 45) # curseur représentant la posx sur une échelle de  à posxmax
+   w.create_line(posxdisp, 25, posxdisp, 45)
    angletotrest = cacheData["angletotal"]
    angleinterrest = cacheData["angleinter"]
 
    coord1 = 1, 50, 200, 170
    coord2 = 1, 60, 190, 160
-   arctot = w.create_arc(coord1, start=10, extent=cacheData["angletotal"], fill="white") #arc de cercle dont l'angle d'extension est proportionnel à : angle renseigné [360]
+   arctot = w.create_arc(coord1, start=10, extent=cacheData["angletotal"], fill="white")
    arcinter = w.create_arc(coord2, start=20, extent=cacheData["angleinter"], fill="black")
 
    print("canvas actualisé avec succès !")
 
-##panneau  translation
+##panneau  trans
 translatif = LabelFrame(root, text="Module translatif")
 translatif.pack(fill="both", expand="yes", side=TOP)
 
 left = Label(translatif, text="Position linéaire :")
-left.pack() #on intègre le module déclaré à sa fenêtre (pack(sans paramètre) donc simplement à la suite du reste)
+left.pack()
 
 saisieposx = Spinbox(translatif, from_=0, to=100,)
-saisieposx.pack() #on intègre le module déclaré à sa fenêtre (pack(sans paramètre) donc simplement à la suite du reste)
+saisieposx.pack()
+
+#posx = saisieposx.get()
 
 ##panneau  rotat°
 rotatif = LabelFrame(root, text="Module rotatif")
@@ -232,10 +211,10 @@ subrotatif1 = Frame(rotatif)
 subrotatif1.pack(fill="both", expand="yes", side=TOP)
 
 titreangleinter = Label(subrotatif1, text="angle entre deux positions (degrés) :")
-titreangleinter.pack() #on intègre le module déclaré à sa fenêtre (pack(sans paramètre) donc simplement à la suite du reste)
+titreangleinter.pack()
 
 saisieangleinter = Spinbox(subrotatif1, from_=0, to=50)
-saisieangleinter.pack() #on intègre le module déclaré à sa fenêtre (pack(sans paramètre) donc simplement à la suite du reste)
+saisieangleinter.pack()
 #cacheData["angleinter"] = saisieangleinter.get()
 
    ## pann subrotat2
@@ -243,12 +222,20 @@ subrotatif2 = Frame(rotatif)
 subrotatif2.pack(fill="both", expand="yes", side=TOP)
 
 titreangletotal = Label(subrotatif2, text="angle total du mouvement (degrés) :")
-titreangletotal.pack() #on intègre le module déclaré à sa fenêtre (pack(sans paramètre) donc simplement à la suite du reste)
+titreangletotal.pack()
 
 saisieangletotal = Spinbox(subrotatif2, from_=0, to=50)
-saisieangletotal.pack() #on intègre le module déclaré à sa fenêtre (pack(sans paramètre) donc simplement à la suite du reste)
+saisieangletotal.pack()
+
+#cacheData["angletotal"] = saisieangletotal.get()
+
+
+
+
 
 checkbutton= Button(root, text="Verifier", command=verifbutton)
-checkbutton.pack() #on intègre le module déclaré à sa fenêtre (pack(sans paramètre) donc simplement à la suite du reste)
+checkbutton.pack()
 
+
+#verifbutton()
 root.mainloop()
--- lib/datasheets.py
+++ lib/datasheets.py
@@ -3,31 +3,37 @@ import pickle
 
 def pickwrite(cacheData):
 	try:
-		filepath = asksaveasfilename(title="Sauveagrder une configuration",filetypes=[('configuration super pano','.supano'),('all files','.*')]) #récupération de l'adresse à laquelle enregistrer le fichier
-		with open(filepath, 'wb') as Fichier: #ouverture d'un Fichier , il sera automatiquement fermé à la fin de la boucle
+		filepath = asksaveasfilename(title="Sauveagrder une configuration",filetypes=[('configuration super pano','.supano'),('all files','.*')])
+		with open(filepath, 'wb') as Fichier:
 			mon_pickler = pickle.Pickler(Fichier)
-			mon_pickler.dump(cacheData)#ecriture des données dans le fichier
+			mon_pickler.dump(cacheData)
 		print('ecriture du fichier  {}  effectuée avec succès.'.format(filepath))
-
+		
 	except:
 		print("ecriture du fichier  {}  impossible !".format(filepath))
-
+	
 def pickread():
-	global appVersion #réccupération de la variable globale (pour écriture)
+	global appVersion
 	try:
-		filepath = askopenfilename(title="Ouvrir une configuration",filetypes=[('configuration super pano','.supano'),('all files','.*')])#récupération de l'adresse à laquelle ouvrir le fichier
-		Fichier = open(filepath, 'rb')
-		cacheData = pickle.load(Fichier) #récupération
-		Fichier.close() #fermeture du fichier
-
-	except: #en cas d'érreur queconque
+		filepath = askopenfilename(title="Ouvrir une configuration",filetypes=[('configuration super pano','.supano'),('all files','.*')])
+		Fichier = open(filepath, 'rb') 
+		cacheData = pickle.load(Fichier) 
+		Fichier.close()
+	except:
 		print("lecture du fichier  {}  impossible".format(filepath))
 		cacheData = {
 		"angleinter":    0,
 		"angletotal":   0,
 		"posx":   0,
 		"versys":   "fail!",
-		} #valeurs par défaut
+		}
 	else:
 		print("lecture du fichier  {}  effectuée avec succès !".format(filepath))
-	return cacheData #retourne la variable quand elle est appelée
+	return cacheData
+#pick()
+
+
+
+
+
+
